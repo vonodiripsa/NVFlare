7f026ff821cf6cf90dc1340723cb4b10040ce35c
Zhihong Zhang <zhihongz@nvidia.com>
2023-11-13 23:23:20 -0500

    Rewrote redis driver to support multiple sessions

--- nvflare/fuel/f3/drivers/aio_redis_driver.py
+++ nvflare/fuel/f3/drivers/aio_redis_driver.py
@@ -11,31 +11,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import asyncio
 import os
 import logging
 import time
-from ast import literal_eval
-from concurrent.futures import CancelledError
-import threading
 from typing import Any, Dict, List
 from asyncio import CancelledError, IncompleteReadError
 import uuid
 import aioredis
-import ssl
 from nvflare.fuel.f3.connection import BytesAlike, Connection
 from nvflare.fuel.f3.comm_error import CommError
 from nvflare.fuel.f3.drivers.aio_context import AioContext
 from nvflare.fuel.f3.drivers.base_driver import BaseDriver
 from nvflare.fuel.f3.drivers.connector_info import ConnectorInfo, Mode
 from nvflare.fuel.f3.drivers.driver_params import DriverCap, DriverParams
-from nvflare.fuel.f3.drivers.net_utils import get_ssl_context
 from nvflare.fuel.f3.drivers.net_utils import get_tcp_urls
 from nvflare.fuel.f3.drivers.net_utils import parse_url
 from nvflare.fuel.utils import fobs
 from nvflare.security.logging import secure_format_exception
-from cryptography import x509
-from cryptography.hazmat.backends import default_backend
 
 CONN_TIMEOUT = 10
 
@@ -43,16 +35,14 @@ log = logging.getLogger(__name__)
 
 
 class RedisConnection(Connection):
-    def __init__(self, redis: Any, server_queue: str, client_queue: str, aio_context: AioContext, connector: ConnectorInfo,
-                 secure: bool):
+    def __init__(self, redis: Any, read_queue: str, write_queue: str, aio_context: AioContext,
+                 connector: ConnectorInfo):
         super().__init__(connector)
-        # self.url = redis_url
         self.redis = redis
-        self.server_queue = server_queue
-        self.client_queue = client_queue
+        self.read_queue = read_queue
+        self.write_queue = write_queue
         self.aio_context = aio_context
         self.closing = False
-        self.secure = secure
         self.conn_props = self._get_aio_properties()
 
     def get_conn_properties(self) -> dict:
@@ -66,51 +56,18 @@ class RedisConnection(Connection):
         self.aio_context.run_coro(self.redis.close())
 
     def _get_aio_properties(self) -> dict:
-        conn_props = {}
-        conn_props[DriverParams.PEER_ADDR.value] = self._get_redis_connection_info(self.redis).get("remote_address", None)
-        conn_props[DriverParams.LOCAL_ADDR.value] = self._get_redis_connection_info(self.redis).get("local_address", None)
-        conn_props[DriverParams.PEER_CN.value] = self._get_redis_connection_info(self.redis).get("peer_cert_name", None)
+        conn_props = {
+            DriverParams.PEER_ADDR.value: "Unknown",
+            DriverParams.LOCAL_ADDR.value: "Unknown",
+            DriverParams.PEER_CN.value: None}
         return conn_props
-
-    def _get_redis_connection_info(self, redis):
-        # context = ssl.create_default_context(cafile='/path/to/your/ssl/ca.pem')
-        # Get peer certificate
-        # ssl_object = redis.connection._transport.get_extra_info('ssl_object')
-        peer_common_name = "Unknown"
-        #
-        # if ssl_object:
-        #     peer_cert = ssl_object.getpeercert(binary=True)
-        #     peer_cert = x509.load_der_x509_certificate(peer_cert, default_backend())
-        #     peer_subject = peer_cert.subject
-        #     peer_common_name = next(
-        #         (
-        #             attr.value
-        #             for attr in peer_subject
-        #             if attr.oid == x509.oid.NameOID.COMMON_NAME
-        #         ),
-        #         None
-        #     )
-        # else:
-        #     peer_common_name = "Unknown"
-        # Get local address and remote address
-        # local_address = redis.connection._transport.get_extra_info('sockname')
-        # remote_address = redis.connection._transport.get_extra_info('peername')
-        local_address = "local_addr"
-        remote_address = "remote_addr"
-        # redis.close()
-        # await redis.wait_closed()
-        return {
-            'local_address': local_address,
-            'remote_address': remote_address,
-            'peer_common_name': peer_common_name,
-            'peer_cert_name': peer_common_name
-        }
 
     async def read_loop(self):
         try:
             while not self.closing:
                 frame = await self._async_read_frame()
-                self.process_frame(frame)
+                if frame:
+                    self.process_frame(frame)
         except IncompleteReadError:
             if log.isEnabledFor(logging.DEBUG):
                 closer = "locally" if self.closing else "by peer"
@@ -124,21 +81,14 @@ class RedisConnection(Connection):
         try:
             if not isinstance(frame, bytes):
                 frame = bytes(frame)
-            await self.redis.rpush(self.client_queue, frame)
+            await self.redis.rpush(self.write_queue, frame)
         except Exception as ex:
             if not self.closing:
                 log.error(f"Error sending frame for connection {self}: {secure_format_exception(ex)}")
 
     async def _async_read_frame(self):
-        # ssl_context = ssl.create_default_context(cafile='/path/to/your/ssl/ca.pem')
-        # redis = await aioredis.create_redis(
-        #     "rediss://your.redis.host:6380",
-        #     encoding="utf-8",
-        #     ssl=self.ssl_context
-        # )
-        # value = await self.redis.rpop(self.conn_name, 1)
         while not self.closing:
-            value = await self.redis.blpop(self.server_queue, timeout=1)
+            value = await self.redis.blpop(self.read_queue, timeout=1)
             if value:
                 _, second = value
                 if second:
@@ -146,12 +96,13 @@ class RedisConnection(Connection):
 
 
 class AioRedisDriver(BaseDriver):
-    # ssl_context = ssl.create_default_context(cafile='/path/to/your/ssl/ca.pem')
+
     def __init__(self):
         super().__init__()
         self.aio_ctx = AioContext.get_global_context()
         self.ssl_context = None
         self.stopping = False
+        self.futures = []
 
     @staticmethod
     def supported_transports() -> List[str]:
@@ -213,21 +164,8 @@ class AioRedisDriver(BaseDriver):
         await coroutine
 
     async def _connect(self, host, port, channel):
-        self.ssl_context = get_ssl_context(self.connector.params, ssl_server=False)
+
         conn_name = str(uuid.uuid4())
-
-        # redis = await aioredis.create_redis(
-        #             (host, port),
-        #             ssl=self.ssl_context
-        #             )
-        
-        # redis = await aioredis.from_url(
-        #             f"redis://{host}:{port}",
-        #             # encoding="utf-8",
-        #             # ssl=self.ssl_context,
-        #             # db=0
-        #         )
-
         redis = None
         try:
             redis = await aioredis.from_url(
@@ -247,9 +185,9 @@ class AioRedisDriver(BaseDriver):
                     time.sleep(0.1)
                     continue
                 else:
-                    server_queue = conn_name + "_in"
-                    client_queue = conn_name + "_out"
-                    await self._create_connection(server_queue, client_queue, host, port)
+                    read_queue = conn_name + "_A"
+                    write_queue = conn_name + "_B"
+                    await self._create_connection(read_queue, write_queue, host, port)
                     break
         except Exception as ex:
             log.error(f"Redis error for connection {conn_name}: {ex}")
@@ -258,12 +196,6 @@ class AioRedisDriver(BaseDriver):
                 await redis.close()
 
     async def _listen(self, host, port, channel):
-        self.ssl_context = get_ssl_context(self.connector.params, ssl_server=True)
-        
-        # redis = await aioredis.create_redis(
-        #             (host, port),
-        #             ssl=self.ssl_context
-        # )
 
         redis = None
         conn_name = "None"
@@ -272,63 +204,41 @@ class AioRedisDriver(BaseDriver):
                         f"redis://{host}:{port}",
                     )
 
-            # redis = await aioredis.create_redis(
-            #             (host, port),
-            #             ssl=self.ssl_context
-            #             )
-
-            # redis = await aioredis.from_url(
-            #     f"rediss://{host}:{port}"
-            #     # encoding="utf-8",
-            #     # ssl=self.ssl_context,
-            #     # db=0
-            # )
             while not self.stopping:
                 value = await redis.blpop(channel, timeout=1)
                 if not value:
                     continue
                 _, buf = value
-                timestamp, conn_name = fobs.loads(buf)
+                conn_name, timestamp = fobs.loads(buf)
                 if time.time() - timestamp > CONN_TIMEOUT:
                     log.debug(f"Connection {conn_name} is too old, it has expired after {CONN_TIMEOUT} seconds")
                     continue
 
-                # The listener side should reverse in/out
-                server_queue = conn_name + "_out"
-                client_queue = conn_name + "_in"
-                await self._create_connection(server_queue, client_queue, host, port)
+                # The listener side should reverse read/write queue
+                read_queue = conn_name + "_B"
+                write_queue = conn_name + "_A"
+                coro = self._create_connection(read_queue, write_queue, host, port)
+                loop = self.aio_ctx.get_event_loop()
+                future = loop.create_task(coro)
+                self.futures.append(future)
+
         except Exception as ex:
             log.error(f"Error listening for connection {conn_name}: {ex}")
         finally:
             if redis:
                 await redis.close()
 
-    async def _create_connection(self, server_queue, client_queue, host, port):
-        
-        redis = await aioredis.from_url(
-                    f"redis://{host}:{port}",
-                )           
-        
-        # redis = await aioredis.create_redis(
-        #             (host, port),
-        #             ssl=self.ssl_context
-        # )
-        
-        # redis = await aioredis.create_redis(
-        #     (host, port),
-        #     ssl=self.ssl_context
-        #     )
-        
-        
-        # redis = await aioredis.from_url(
-        #     f"rediss://{host}:{port}"
-        #     # encoding="utf-8",
-        #     # ssl=self.ssl_context,
-        #     # db=0
-        # )
-        sec = self.ssl_context is not None
-        conn = RedisConnection(redis, server_queue, client_queue, self.aio_ctx, self.connector, sec)
-        self.add_connection(conn)
-        await conn.read_loop()
-        self.close_connection(conn)
+    async def _create_connection(self, read_queue, write_queue, host, port):
+
+        try:
+            redis = await aioredis.from_url(
+                        f"redis://{host}:{port}",
+                    )
+
+            conn = RedisConnection(redis, read_queue, write_queue, self.aio_ctx, self.connector)
+            self.add_connection(conn)
+            await conn.read_loop()
+            self.close_connection(conn)
+        except Exception as ex:
+            log.error("Connection {conn} closed due to error: {ex")
   
